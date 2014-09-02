22ee9c602ee639dd2af090ec1f7942e46143471b
Minke Zhang <minke.zhang@gmail.com>
2014-09-02 13:59:23 -0500

    added test skeleton

--- src/main.cc
+++ src/main.cc
@@ -6,43 +6,4 @@
 #include <memory>
 
 int main() {
-	remove("example.txt");
-
-	std::shared_ptr<giga::File> f (new giga::File("example.txt", "rw+"));
-	std::shared_ptr<giga::Client> c = f->open();
-
-	// write to the end of the file
-	c->write("Hello world!");
-
-	c->seek(0, true, true);
-	std::cout << c->read(100) << std::endl;
-
-	// overwrite portions of a file
-	c->seek(6, true, true);
-	c->write("earth");
-
-	c->seek(0, true, true);
-	std::cout << c->read(100) << std::endl;
-
-	// erase "Hello"
-	c->seek(0, true, true);
-	c->erase(5);
-
-	c->seek(0, true, true);
-	std::cout << c->read(100) << std::endl;
-
-	c->close();
-	f->save();
-
-	// reopen the file at the beginning of the stream
-	c->open();
-
-	// insert into the file
-	c->write("I love the", true);
-
-	c->seek(0, true, true);
-	std::cout << c->read(100) << std::endl;
-
-	c->close();
-	f->save();
 }
