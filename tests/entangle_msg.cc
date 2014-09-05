cc5745034649f473d316e06b0e7e3940d9c97186
Minke Zhang <minke.zhang@gmail.com>
2014-09-05 12:43:11 -0500

    added message constructor tests

--- tests/entangle_msg.cc
+++ tests/entangle_msg.cc
@@ -6,4 +6,9 @@
 TEST_CASE("entangle|entangle_msg") {
 	auto m = entangle::EntangleMessage(":182:::CONN::foobar123");
         REQUIRE(m.to_string().compare("0:182:::CONN:0:foobar123") == 0);
+	REQUIRE(m.get_ack() == 0);
+	REQUIRE(m.get_msg_id() == 182);
+	REQUIRE(m.get_err() == 0);
+
+	REQUIRE_THROWS_AS(entangle::EntangleMessage(":182:::CONN:foobar123"), exceptionpp::InvalidOperation);
 }
