c9855cf5f3b3205cd3cbd7c8405deb9102e0e7d8
Minke Zhang <minke.zhang@gmail.com>
2014-09-19 14:27:17 -0500

    added more tests

--- tests/dopt_node.cc
+++ tests/dopt_node.cc
@@ -14,6 +14,76 @@ TEST_CASE("entangle|dopt_node-enc") {
 	REQUIRE(n.cmp_upd_t(n.dec_upd_t("1:100:c"), { entangle::del, 100, 'c' }) == true);
 }
 
+
+TEST_CASE("entangle|dopt_node-convergence") {
+	auto s = entangle::OTNode(8000, 100);
+	auto x = entangle::OTNode(8050, 2);
+
+	REQUIRE_NOTHROW(s.up());
+	REQUIRE_NOTHROW(x.up());
+	REQUIRE(x.join("localhost", 8000) == true);
+	sleep(1);
+
+	CHECK(s.get_context().compare("") == 0);
+	CHECK(x.get_context().compare(s.get_context()) == 0);
+
+	REQUIRE(x.ins(0, '1') == true);
+	sleep(1);
+	CHECK(s.get_context().compare("1") == 0);
+	CHECK(x.get_context().compare(s.get_context()) == 0);
+
+	REQUIRE(x.ins(0, '1') == true);
+	sleep(1);
+	CHECK(s.get_context().compare("11") == 0);
+	CHECK(x.get_context().compare(s.get_context()) == 0);
+
+	REQUIRE(x.ins(0, '2') == true);
+	sleep(1);
+	CHECK(s.get_context().compare("211") == 0);
+	CHECK(x.get_context().compare(s.get_context()) == 0);
+
+	REQUIRE(x.del(1) == true);
+	sleep(1);
+	CHECK(s.get_context().compare("21") == 0);
+	CHECK(x.get_context().compare(s.get_context()) == 0);
+
+	REQUIRE(x.drop("localhost", 8000) == true);
+	sleep(1);
+	REQUIRE_NOTHROW(s.dn());
+	REQUIRE_NOTHROW(x.dn());
+}
+
+TEST_CASE("entangle|dopt_node-daemon") {
+	auto n = entangle::OTNode(8888, 100);
+	auto m = entangle::OTNode(8889, 100);
+
+	REQUIRE(m.join("localhost", 8888) == false);
+
+	REQUIRE_NOTHROW(n.up());
+	REQUIRE_NOTHROW(n.dn());
+
+	REQUIRE_NOTHROW(n.up());
+	REQUIRE_NOTHROW(m.up());
+	REQUIRE(m.join("localhost", 8888) == true);
+	sleep(1);
+	REQUIRE(m.size() == 1);
+	REQUIRE(n.size() == 1);
+	REQUIRE(m.join("localhost", 8888) == false);
+	REQUIRE(n.join("localhost", 8889) == false);
+	REQUIRE(m.size() == 1);
+	REQUIRE(n.size() == 1);
+	REQUIRE(m.drop("localhost", 8888) == true);
+	sleep(1);
+	REQUIRE(m.size() == 0);
+	REQUIRE(n.size() == 0);
+	REQUIRE_NOTHROW(m.dn());
+	REQUIRE_NOTHROW(n.dn());
+
+	// auto-call OTNode::dn on stack unwind
+	REQUIRE_NOTHROW(n.up());
+	sleep(1);
+}
+
 TEST_CASE("entangle|dopt_node-topography") {
 	auto s = entangle::OTNode(8000, 100);
 	auto x = entangle::OTNode(8050, 2);
@@ -130,35 +200,4 @@ TEST_CASE("entangle|dopt_node-concurrent") {
 	REQUIRE_NOTHROW(s.dn());
 	REQUIRE_NOTHROW(x.dn());
 	REQUIRE_NOTHROW(y.dn());
-}
-
-TEST_CASE("entangle|dopt_node-daemon") {
-	auto n = entangle::OTNode(8888, 100);
-	auto m = entangle::OTNode(8889, 100);
-
-	REQUIRE(m.join("localhost", 8888) == false);
-
-	REQUIRE_NOTHROW(n.up());
-	REQUIRE_NOTHROW(n.dn());
-
-	REQUIRE_NOTHROW(n.up());
-	REQUIRE_NOTHROW(m.up());
-	REQUIRE(m.join("localhost", 8888) == true);
-	sleep(1);
-	REQUIRE(m.size() == 1);
-	REQUIRE(n.size() == 1);
-	REQUIRE(m.join("localhost", 8888) == false);
-	REQUIRE(n.join("localhost", 8889) == false);
-	REQUIRE(m.size() == 1);
-	REQUIRE(n.size() == 1);
-	REQUIRE(m.drop("localhost", 8888) == true);
-	sleep(1);
-	REQUIRE(m.size() == 0);
-	REQUIRE(n.size() == 0);
-	REQUIRE_NOTHROW(m.dn());
-	REQUIRE_NOTHROW(n.dn());
-
-	// auto-call OTNode::dn on stack unwind
-	REQUIRE_NOTHROW(n.up());
-	sleep(1);
 }
